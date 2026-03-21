f6fef385b3a4d0fa3e3af7e6fed21e152983166c
Pigalala <w8888mc@gmail.com>
2026-03-21 17:15:03 +0000

    Javadocs zzz

--- src/main/java/dev/piggle/filtermap/FilterMap.java
+++ src/main/java/dev/piggle/filtermap/FilterMap.java
@@ -7,10 +7,17 @@ import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Gatherer;
 
+/// Filter map implementation.
+///
+/// Also contains static factory methods.
+///
+/// @param <T> Input type of mapping function
+/// @param <U> Output type of mapping function
 public final class FilterMap<T, U> implements Gatherer<T, Void, U> {
 
     private final Function<T, @Nullable U> mapper;
 
+    /// @param mapper The mapper function from T to U. An output (U) is discarded if null
     public FilterMap(Function<T, @Nullable U> mapper) {
         this.mapper = mapper;
     }
@@ -32,14 +39,19 @@ public final class FilterMap<T, U> implements Gatherer<T, Void, U> {
         };
     }
 
+    /// Creates a FilterMap instance from a mapping function.
+    /// @see FilterMap#FilterMap(Function)
     public static <T, U> FilterMap<T, U> of(Function<T, @Nullable U> mapper) {
         return new FilterMap<>(mapper);
     }
 
+    /// Creates a FilterMap instance where empty optionals are discarded
     public static <T, U> FilterMap<T, U> filterOptional(Function<T, Optional<U>> mapper) {
         return new FilterMap<>(in -> mapper.apply(in).orElse(null));
     }
 
+    /// Creates a FilterMap instance where instances of a given class are mapped, the rest are discarded.
+    /// @param target The class that each object will be after the map
     @SuppressWarnings("unchecked")
     public static <T, U> FilterMap<T, U> filterInstanceOf(Class<U> target) {
         return new FilterMap<>(in -> {
